636b6c850e81db4169bf7a09f8e8d62e1c24f916
Duncan McGreggor <oubiwann@gmail.com>
2021-11-27 18:49:45 -0600

    Added system.

--- cmd/tut/main.go
+++ cmd/tut/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"image/color"
+	"log"
+
 	"github.com/EngoEngine/ecs"
 	"github.com/EngoEngine/engo"
 	"github.com/EngoEngine/engo/common"
 )
 
 type myScene struct{}
+
+type City struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
 
 // Type uniquely defines your game type
 func (*myScene) Type() string { return "myGame" }
@@ -20,8 +29,30 @@ func (*myScene) Preload() {
 // Setup is called before the main loop starts. It allows you
 // to add entities and systems to your Scene.
 func (*myScene) Setup(u engo.Updater) {
+	common.SetBackground(color.White)
+	city := City{BasicEntity: ecs.NewBasic()}
+	city.SpaceComponent = common.SpaceComponent{
+		Position: engo.Point{10, 10},
+		Width:    303,
+		Height:   641,
+	}
+	texture, err := common.LoadedSprite("textures/city.png")
+	if err != nil {
+		log.Println("Unable to load texture: " + err.Error())
+	}
+
+	city.RenderComponent = common.RenderComponent{
+		Drawable: texture,
+		Scale:    engo.Point{1, 1},
+	}
 	world, _ := u.(*ecs.World)
 	world.AddSystem(&common.RenderSystem{})
+	for _, system := range world.Systems() {
+		switch sys := system.(type) {
+		case *common.RenderSystem:
+			sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
+		}
+	}
 }
 
 func main() {
