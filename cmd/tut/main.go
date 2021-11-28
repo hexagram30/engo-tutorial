880faf3cc3c83e17bd880c7d88895a912cb56a86
Duncan McGreggor <oubiwann@gmail.com>
2021-11-27 19:14:18 -0600

    Added systems package.

--- cmd/tut/main.go
+++ cmd/tut/main.go
@@ -2,57 +2,34 @@ package main
 
 import (
 	"image/color"
-	"log"
 
 	"github.com/EngoEngine/ecs"
 	"github.com/EngoEngine/engo"
 	"github.com/EngoEngine/engo/common"
+
+	"github.com/hexagram30/engo-tutorial/pkg/systems"
 )
 
-type myScene struct{}
-
-type City struct {
-	ecs.BasicEntity
-	common.RenderComponent
-	common.SpaceComponent
-}
+type myGame struct{}
 
 // Type uniquely defines your game type
-func (*myScene) Type() string { return "myGame" }
+func (*myGame) Type() string { return "myGame" }
 
-// Preload is called before loading any assets from the disk,
-// to allow you to register / queue them
-func (*myScene) Preload() {
+// Preload is called before loading any assets from the disk, to allow you to
+// register / queue them
+func (*myGame) Preload() {
 	engo.Files.Load("textures/city.png")
 }
 
-// Setup is called before the main loop starts. It allows you
-// to add entities and systems to your Scene.
-func (*myScene) Setup(u engo.Updater) {
-	common.SetBackground(color.White)
-	city := City{BasicEntity: ecs.NewBasic()}
-	city.SpaceComponent = common.SpaceComponent{
-		Position: engo.Point{10, 10},
-		Width:    303,
-		Height:   641,
-	}
-	texture, err := common.LoadedSprite("textures/city.png")
-	if err != nil {
-		log.Println("Unable to load texture: " + err.Error())
-	}
-
-	city.RenderComponent = common.RenderComponent{
-		Drawable: texture,
-		Scale:    engo.Point{1, 1},
-	}
+// Setup is called before the main loop starts. It allows you to add entities
+// and systems to your Scene.
+func (*myGame) Setup(u engo.Updater) {
 	world, _ := u.(*ecs.World)
+	engo.Input.RegisterButton("AddCity", engo.KeyF1)
+	common.SetBackground(color.White)
 	world.AddSystem(&common.RenderSystem{})
-	for _, system := range world.Systems() {
-		switch sys := system.(type) {
-		case *common.RenderSystem:
-			sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
-		}
-	}
+	world.AddSystem(&common.MouseSystem{})
+	world.AddSystem(&systems.CityBuildingSystem{})
 }
 
 func main() {
@@ -61,5 +38,5 @@ func main() {
 		Width:  400,
 		Height: 400,
 	}
-	engo.Run(opts, &myScene{})
+	engo.Run(opts, &myGame{})
 }
---
+++ pkg/systems/city.go
@@ -0,0 +1,72 @@
+package systems
+
+import (
+	"fmt"
+
+	"github.com/EngoEngine/ecs"
+	"github.com/EngoEngine/engo"
+	"github.com/EngoEngine/engo/common"
+)
+
+type City struct {
+	ecs.BasicEntity
+	common.RenderComponent
+	common.SpaceComponent
+}
+
+type MouseTracker struct {
+	ecs.BasicEntity
+	common.MouseComponent
+}
+
+type CityBuildingSystem struct {
+	world        *ecs.World
+	mouseTracker MouseTracker
+}
+
+// Remove is called whenever an Entity is removed from the World, in order to
+// remove it from this system as well
+func (*CityBuildingSystem) Remove(ecs.BasicEntity) {}
+
+// Update is ran every frame, with `dt` being the time in seconds since the
+// last frame
+func (cb *CityBuildingSystem) Update(dt float32) {
+	if engo.Input.Button("AddCity").JustPressed() {
+		fmt.Println("The gamer pressed F1")
+		city := City{BasicEntity: ecs.NewBasic()}
+		city.SpaceComponent = common.SpaceComponent{
+			Position: engo.Point{cb.mouseTracker.MouseX, cb.mouseTracker.MouseY},
+			Width:    30,
+			Height:   64,
+		}
+		texture, err := common.LoadedSprite("textures/city.png")
+		if err != nil {
+			panic("Unable to load texture: " + err.Error())
+		}
+		city.RenderComponent = common.RenderComponent{
+			Drawable: texture,
+			Scale:    engo.Point{X: 0.1, Y: 0.1},
+		}
+		for _, system := range cb.world.Systems() {
+			switch sys := system.(type) {
+			case *common.RenderSystem:
+				sys.Add(&city.BasicEntity, &city.RenderComponent, &city.SpaceComponent)
+			}
+		}
+	}
+}
+
+// New is the initialisation of the System
+func (cb *CityBuildingSystem) New(w *ecs.World) {
+	cb.world = w
+	fmt.Println("CityBuildingSystem was added to the Scene")
+	cb.mouseTracker.BasicEntity = ecs.NewBasic()
+	cb.mouseTracker.MouseComponent = common.MouseComponent{Track: true}
+
+	for _, system := range w.Systems() {
+		switch sys := system.(type) {
+		case *common.MouseSystem:
+			sys.Add(&cb.mouseTracker.BasicEntity, &cb.mouseTracker.MouseComponent, nil, nil)
+		}
+	}
+}
